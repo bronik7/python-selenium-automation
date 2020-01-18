4ca4a68c73d73924ed8d8f08a49cfbd3257f9572
bronik7 <b_z94102@yahoo.com>
2020-01-18 11:06:19 -0800

    no message

--- features/steps/product_search.py
+++ features/steps/product_search.py
@@ -21,7 +21,7 @@ def input_search(context, search_word):
     search.send_keys(search_word)
     sleep(4)
 
-cXcc
+
 @when('Click on search icon')
 def click_search_icon(context):
     context.driver.find_element(*SEARCH_SUBMIT).click()
